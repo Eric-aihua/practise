100016ee20f363c07ee74fdc02c58e5b9416cbd4
Eric.aihua <eric.sunah@gmail.com>
2014-11-26 11:25:40 +0800

    single node test
    single node main class and test data

---
+++ hadoop/src/main/java/com/eric/hadoop/jobconfig/MaxTemperature.java
@@ -0,0 +1,36 @@
+package com.eric.hadoop.jobconfig;
+
+import java.io.IOException;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+
+import com.eric.hadoop.map.MaxTemperatureMapper;
+import com.eric.hadoop.reduce.MaxTemperatureReduce;
+
+
+public class MaxTemperature {
+  public static void main(String[] args) throws IOException {
+    JobConf conf = new JobConf(MaxTemperature.class);
+    conf.setJobName("Get Max Temperature!");
+    if (args.length != 2) {
+      System.err.print("Must contain 2 params:inputPath OutputPath");
+      System.exit(0);
+    }
+
+    FileInputFormat.addInputPaths(conf, args[0]);
+    FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+
+    conf.setMapperClass(MaxTemperatureMapper.class);
+    conf.setReducerClass(MaxTemperatureReduce.class);
+
+    conf.setOutputKeyClass(Text.class);
+    conf.setOutputValueClass(IntWritable.class);
+    JobClient.runJob(conf);
+  }
+}
