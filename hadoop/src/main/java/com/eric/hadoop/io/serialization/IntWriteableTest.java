0ac490ddd7d602a8862dbf3ea70f1b23553f7996
Eric.aihua <eric.sunah@gmail.com>
2014-12-04 16:49:15 +0800

    add serialize test code

---
+++ hadoop/src/main/java/com/eric/hadoop/io/serialization/IntWriteableTest.java
@@ -0,0 +1,75 @@
+package com.eric.hadoop.io.serialization;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.util.StringUtils;
+
+/**
+ * 验证IntWritable 序列化
+ * @author Eric.sunah
+ * 2014年12月4日
+ *
+ */
+public class IntWriteableTest {
+  public static void main(String[] args) {
+    int inputValue = 163;
+    IntWritable intWritable = new IntWritable(inputValue);
+    // output serialize result
+    System.out.println(testSerialization(intWritable));
+    // verify deserialize result
+    System.out.println(testDeSerialization(serialization(intWritable)) == inputValue);
+  }
+
+  private static String testSerialization(IntWritable intWritable) {
+    return StringUtils.byteToHexString(serialization(intWritable));
+  }
+
+  private static int testDeSerialization(byte[] bytes) {
+    return deSerialization(bytes);
+  }
+
+  /**
+   * 对某个writable进行序列化，返回字节数组
+   * 
+   * @param writable
+   * @return
+   */
+  public static byte[] serialization(Writable writable) {
+    ByteArrayOutputStream byteOutputStream = new ByteArrayOutputStream();
+    DataOutputStream os = new DataOutputStream(byteOutputStream);
+    try {
+      writable.write(os);
+      os.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return byteOutputStream.toByteArray();
+  }
+
+
+
+  /**
+   * 对字节进行反序列化
+   * 
+   * @param bytes
+   * @return
+   */
+  public static int deSerialization(byte[] bytes) {
+    ByteArrayInputStream byteOutputStream = new ByteArrayInputStream(bytes);
+    DataInputStream os = new DataInputStream(byteOutputStream);
+    IntWritable iw = new IntWritable();
+    try {
+      iw.readFields(os);
+      os.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return iw.get();
+  }
+}
