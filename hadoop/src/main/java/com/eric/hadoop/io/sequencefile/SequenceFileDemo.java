047a0909047673e253f34471b895670df4fc476a
Eric-aihua <sunaihua123@126.com>
2014-12-10 22:20:10 +0800

    add comments for sequence file demo

--- hadoop/src/main/java/com/eric/hadoop/io/sequencefile/SequenceFileDemo.java
+++ hadoop/src/main/java/com/eric/hadoop/io/sequencefile/SequenceFileDemo.java
@@ -16,83 +16,92 @@ import org.apache.hadoop.io.Text;
  * 
  */
 public class SequenceFileDemo {
-
-  private static final String OPERA_FILE = "./output.seq";
-  /**
-   * 随便从网上截取的一段文本
-   */
-  private static String[] testArray = {
-      "<plugin>                                                                     ",
-      "  <groupId>org.apache.avro</groupId>                                         ",
-      "  <artifactId>avro-maven-plugin</artifactId>                                 ",
-      "  <version>1.7.7</version>                                                   ",
-      "  <executions>                                                               ",
-      "    <execution>                                                              ",
-      "      <phase>generate-sources</phase>                                        ",
-      "      <goals>                                                                ",
-      "        <goal>schema</goal>                                                  ",
-      "      </goals>                                                               ",
-      "      <configuration>                                                        ",
-      "        <sourceDirectory>${project.basedir}/src/main/avro/</sourceDirectory> ",
-      "        <outputDirectory>${project.basedir}/src/main/java/</outputDirectory> ",
-      "      </configuration>                                                       ",
-      "    </execution>                                                             ",
-      "  </executions>                                                              ",
-      "</plugin>                                                                    ",
-      "<plugin>                                                                     ",
-      "  <groupId>org.apache.maven.plugins</groupId>                                ",
-      "  <artifactId>maven-compiler-plugin</artifactId>                             ",
-      "  <configuration>                                                            ",
-      "    <source>1.6</source>                                                     ",
-      "    <target>1.6</target>                                                     ",
-      "  </configuration>                                                           ", "</plugin>"};
-
-  public static void main(String[] args) throws IOException {
-    writeSequenceFile(OPERA_FILE);
-    readSequenceFile(OPERA_FILE);
-  }
-
-  private static void readSequenceFile(String inputFile) throws IOException {
-    Configuration config = new Configuration();
-    Path path = new Path(inputFile);
-    SequenceFile.Reader reader = null;
-    try {
-
-      FileSystem fs = FileSystem.get(URI.create(inputFile), config);
-      reader = new SequenceFile.Reader(fs, path, config);
-      IntWritable key = new IntWritable();
-      Text value = new Text();
-      while (reader.next(key, value)) {
-        System.out.printf("\t%s\t%s\n", key, value);
-      }
-    } finally {
-      IOUtils.closeStream(reader);
-    }
-
-  }
-
-  private static void writeSequenceFile(String outputFile) throws IOException {
-    Configuration config = new Configuration();
-    Path path = new Path(outputFile);
-    IntWritable key = new IntWritable();
-    Text value = new Text();
-    SequenceFile.Writer writer = null;
-    try {
-
-      FileSystem fs = FileSystem.get(URI.create(outputFile), config);
-      writer = SequenceFile.createWriter(fs, config, path, key.getClass(), value.getClass());
-      for (int i = 1; i < 2000; i++) {
-        key.set(2000 - i);
-        value.set(testArray[i % testArray.length]);
-        System.out.printf("[%s]\t%s\t%s\n", writer.getLength() + "", key, value);
-        writer.append(key, value);
-      }
-    } finally {
-      IOUtils.closeStream(writer);
-    }
-
-  }
-
-
-
+	
+	private static final String	OPERA_FILE	= "./output.seq";
+	/**
+	 * 随便从网上截取的一段文本
+	 */
+	private static String[]	    testArray	= { "<plugin>                                                                     ",
+	        "  <groupId>org.apache.avro</groupId>                                         ",
+	        "  <artifactId>avro-maven-plugin</artifactId>                                 ",
+	        "  <version>1.7.7</version>                                                   ",
+	        "  <executions>                                                               ",
+	        "    <execution>                                                              ",
+	        "      <phase>generate-sources</phase>                                        ",
+	        "      <goals>                                                                ",
+	        "        <goal>schema</goal>                                                  ",
+	        "      </goals>                                                               ",
+	        "      <configuration>                                                        ",
+	        "        <sourceDirectory>${project.basedir}/src/main/avro/</sourceDirectory> ",
+	        "        <outputDirectory>${project.basedir}/src/main/java/</outputDirectory> ",
+	        "      </configuration>                                                       ",
+	        "    </execution>                                                             ",
+	        "  </executions>                                                              ",
+	        "</plugin>                                                                    ",
+	        "<plugin>                                                                     ",
+	        "  <groupId>org.apache.maven.plugins</groupId>                                ",
+	        "  <artifactId>maven-compiler-plugin</artifactId>                             ",
+	        "  <configuration>                                                            ",
+	        "    <source>1.6</source>                                                     ",
+	        "    <target>1.6</target>                                                     ",
+	        "  </configuration>                                                           ", "</plugin>"};
+	
+	public static void main(String[] args) throws IOException {
+		writeSequenceFile(OPERA_FILE);
+		readSequenceFile(OPERA_FILE);
+	}
+	
+	private static void readSequenceFile(String inputFile) throws IOException {
+		Configuration config = new Configuration();
+		Path path = new Path(inputFile);
+		SequenceFile.Reader reader = null;
+		try {
+			
+			FileSystem fs = FileSystem.get(URI.create(inputFile), config);
+			reader = new SequenceFile.Reader(fs, path, config);
+			IntWritable key = new IntWritable();
+			Text value = new Text();
+			long posion = reader.getPosition();
+			// reader.next()返回非空的话表示正在读，如果返回null表示已经读到文件结尾的地方
+			while (reader.next(key, value)) {
+				//打印同步点的位置信息
+				String syncMark = reader.syncSeen() ? "*" : "";
+				System.out.printf("[%s\t%s]\t%s\t%s\n", posion, syncMark, key, value);
+				posion = reader.getPosition();
+			}
+		} finally {
+			IOUtils.closeStream(reader);
+		}
+		
+	}
+	
+	/**
+	 * 写Sequence File 文件
+	 * 
+	 * @param outputFile
+	 * @throws IOException
+	 */
+	private static void writeSequenceFile(String outputFile) throws IOException {
+		Configuration config = new Configuration();
+		Path path = new Path(outputFile);
+		IntWritable key = new IntWritable();
+		Text value = new Text();
+		SequenceFile.Writer writer = null;
+		try {
+			
+			FileSystem fs = FileSystem.get(URI.create(outputFile), config);
+			writer = SequenceFile.createWriter(fs, config, path, key.getClass(), value.getClass());
+			for (int i = 1; i < 2000; i++) {
+				key.set(2000 - i);
+				value.set(testArray[i % testArray.length]);
+				System.out.printf("[%s]\t%s\t%s\n", writer.getLength() + "", key, value);
+				// 通过Append方法进行写操作
+				writer.append(key, value);
+			}
+		} finally {
+			IOUtils.closeStream(writer);
+		}
+		
+	}
+	
 }
