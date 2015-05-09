6548af12f12c5752f3c785c1ab5548323a5278a9
Eric <eric.sunah@gmail.com>
2015-05-09 15:32:52 +0800

    kafka sample

--- simple-service/src/main/java/com/example/redis/GetRedisInstanceInfo.java
+++ simple-service/src/main/java/com/example/redis/GetRedisInstanceInfo.java
@@ -1,5 +1,7 @@
 package com.example.redis;
 
+import redis.clients.jedis.Jedis;
+
 public class GetRedisInstanceInfo {
 	public static void main(String[] args) {
 		Jedis jedis = new Jedis("10.11.20.140");  
