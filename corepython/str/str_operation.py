9642cb3b42c6e5f2902c28104d24f26520dfe904
Eric <eric.sunah@gmail.com>
2015-05-12 21:47:33 +0800

    add for setup test

---
+++ corepython/str/str_operation.py
@@ -0,0 +1,11 @@
+__author__ = 'aihua.sun'
+
+def not_empty_check(s):
+    return not(s is None) and len(s.strip())>0
+
+
+if __name__=='__main__':
+   print not_empty_check(None)
+   print not_empty_check('')
+   print not_empty_check('   ')
+   print not_empty_check(' eric  ')
