43f5e7aa4dda2d1fe56ae776148a447a66fff7c3
Eric <eric.sunah@gmail.com>
2015-06-25 16:33:46 +0800

    update

--- java_docker_client/dockerclient/src/main/java/eric/dockerclient/DockerConTest.java
+++ java_docker_client/dockerclient/src/main/java/eric/dockerclient/DockerConTest.java
@@ -1,5 +1,12 @@
 package eric.dockerclient;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.LineIterator;
+
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.core.DockerClientBuilder;
 import com.github.dockerjava.core.DockerClientConfig;
@@ -7,16 +14,37 @@ import com.github.dockerjava.core.DockerClientConfig;
 public class DockerConTest {
 	public static void main(String[] args) {
 		DockerClientConfig config = DockerClientConfig.createDefaultConfigBuilder()
-			    .withVersion("1.18")
-			    .withUri("http://10.0.0.136:2375")
-//			    .withUsername("dockeruser")
-//			    .withPassword("ilovedocker")
-//			    .withEmail("dockeruser@github.com")
-//			    .withServerAddress("https://index.docker.io/v1/")
-//			    .withDockerCertPath("/home/user/.docker")
+			    .withUri("http://192.168.40.83:2375")
 			    .build();
 			DockerClient docker = DockerClientBuilder.getInstance(config).build();
 			System.out.println(docker.listImagesCmd().exec());
-			System.out.print(docker.infoCmd().exec());
+			System.out.println(docker.listImagesCmd().exec().size());
+			InputStream is=docker.logContainerCmd("18a3").withStdErr().withStdOut().exec();
+			System.out.println(consumeAsString(is));
+			
+			
 	}
+	
+	public static String consumeAsString(InputStream response)  {
+
+		StringWriter logwriter = new StringWriter();
+
+		try {
+			LineIterator itr = IOUtils.lineIterator(
+					response, "UTF-8");
+
+			while (itr.hasNext()) {
+				String line = itr.next();
+				logwriter.write(line + (itr.hasNext() ? "\n" : ""));
+			}
+			response.close();
+
+			return logwriter.toString();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} finally {
+			IOUtils.closeQuietly(response);
+		}
+	}
+
 }
