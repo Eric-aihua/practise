d8d3f9d792e5c5bb9c713029bd2f2e3e64b6183e
Eric <eric.sunah@gmail.com>
2015-08-26 16:13:18 +0800

    cookbook

---
+++ java_docker_client/dockerclient/src/main/java/eric/dockerclient/ImageTest.java
@@ -0,0 +1,22 @@
+package eric.dockerclient;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.model.AuthConfig;
+import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.core.DockerClientConfig;
+
+/**
+ * @author aihua.sun
+ *
+ */
+public class ImageTest {
+	public static void main(String[] args) {
+		String testImage = "dokk.co/mysql5:latest";
+		DockerClientConfig config = DockerClientConfig.createDefaultConfigBuilder()
+			    .withUri("http://192.168.58.135:2375")
+			    .build();
+			DockerClient docker = DockerClientBuilder.getInstance(config).build();
+			docker.pullImageCmd(testImage).withAuthConfig(new AuthConfig()).exec();
+	}
+
+}
