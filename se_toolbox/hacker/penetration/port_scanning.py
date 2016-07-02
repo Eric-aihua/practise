57c6cf41a8bbe815e855f3723056249acff934ba
eric <eric@eric-desktop.(none)>
2016-07-02 19:34:23 +0800

    update for semphore

--- se_toolbox/hacker/penetration/port_scanning.py
+++ se_toolbox/hacker/penetration/port_scanning.py
@@ -3,25 +3,30 @@
 import optparse
 import os
 import socket
-from threading import activeCount, Thread
+from threading import activeCount, Thread,Semaphore
 import traceback
 
 __author__ = 'eric.sun'
 
+screen_lock=Semaphore(value=1)
 
 def scanning(host, port):
-    os.system('title '+str(port))
+    #os.system('title '+str(port))
     cli_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        indicator = cli_sock.connect_ex((host, port))
-        if indicator == 0:
-            print '[+] host:%s port:%s connect successful\n'%(host,port)
-        else:
-            pass
-            #print '[-] host:%s port:%s connect failed\n'%(host,port)
-        cli_sock.close()
+        #indicator = cli_sock.connect_ex((host, port))
+        cli_sock.connect((host, port))
+        cli_sock.send('VillentPython\r\n')
+	results=cli_sock.recv(100)
+	screen_lock.acquire()
+        print '[+] host:%s port:%s connect successful'%(host,port)
+        print '    [-] con message: %s'%(results)
     except:
-        traceback.print_exc()
+	screen_lock.acquire()
+#        print '[-] host:%s port:%s connect failed'%(host,port)
+    finally:
+	cli_sock.close()
+	screen_lock.release()
 
 
 if __name__ == '__main__':
@@ -38,14 +43,10 @@ if __name__ == '__main__':
         port=1
         while port<65535:
             if activeCount()<200:
-                # print activeCount()
                 Thread(target=scanning,args=(options.full_host,port)).start()
                 port+=1
 
-
-            #scanning(options.full_host,port)
-    else:
+    if not options:
         parser.print_help()
-
 
 
