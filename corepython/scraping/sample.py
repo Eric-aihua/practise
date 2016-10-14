388068c3bc7d1b11011afff4906c74988a17b9f6
Eric <eric.sunah@gmail.com>
2016-10-14 15:55:49 +0800

    update

--- corepython/scraping/sample.py
+++ corepython/scraping/sample.py
@@ -1,11 +1,11 @@
 __author__ = 'Eric'
 import requests
 
+
 def get_message_from_baidu():
     response = requests.get('http://www.baidu.com')
 
     print(response.content);
-
 
 
 get_message_from_baidu()
