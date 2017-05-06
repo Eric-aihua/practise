b07732e0caa74019f237946194108b692e8f7b19
Eric.aihua <eric.sunah@gmail.com>
2017-05-06 16:14:59 +0800

    update

--- python_cookbook/scraping/cet4_grade_query.py
+++ python_cookbook/scraping/cet4_grade_query.py
@@ -1,7 +1,11 @@
 # encoding: utf-8
+import sys
 import urllib2
+import time
 
 import mechanize
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 __author__ = 'eric.sun'
 import urllib
@@ -14,42 +18,60 @@ import urllib
 　　E.最后两位是你的座位号, 也应该能记住吧!
 """
 
-# info_size = 'http://www.chsi.com.cn/cet/query?'
+#info_size = 'http://www.chsi.com.cn/cet/query?'
 info_size = 'http://www.chsi.com.cn/cet'
 school_code = '321640'
 exam_years = ['082', '091']
 exam_code = '1'
-exam_rooms = range(1)
-sit_nums = range(1)
+exam_rooms = range(999)
+sit_nums = range(99)
 student_name = '孙爱华'
 DEFAULT_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:10.0) Gecko/20100101 Firefox/10.0'
 
 
 def guess_grade():
+    browser = mechanize.Browser()
+    browser.set_handle_equiv(True)
+    browser.set_handle_gzip(True)
+    browser.set_handle_redirect(True)
+    browser.set_handle_referer(True)
+    browser.set_handle_robots(False)
+                # browser.set_debug_http(True)
+    browser.open(info_size.encode('utf-8'))            
     for exam_year in exam_years:
         for exam_room in exam_rooms:
             final_exam_room='0'*(3-len(str(exam_room)))+str(exam_room)
             for sit_num in sit_nums:
                 final_sit_num = '0' * (2-len(str(sit_num))) + str(sit_num)
                 exam_num = '%s%s%s%s%s' % (school_code, exam_year, exam_code,final_exam_room, final_sit_num)
-                request_params={'zkzh':exam_num,'xm':student_name}
-                request_url='%s%s' %(info_size,urllib.urlencode(request_params))
-                print '#####',request_url
-                # headers = {'User-agent': DEFAULT_AGENT}
-                # requests = urllib2.Request(request_url, headers=headers)
-                # opener = urllib2.build_opener()
-                # html = opener.open(requests).read()
-                # print html
+                #request_params={'zkzh':exam_num,'xm':student_name}
+                #request_url='%s%s' %(info_size,urllib.urlencode(request_params))
+                #print '#####',request_url
+                #headers = {'User-agent': DEFAULT_AGENT}
+                #requests = urllib2.Request(request_url, headers=headers)
+                #opener = urllib2.build_opener()
+                #html = opener.open(requests).read()
+                #print html
 
-                browser = mechanize.Browser()
-                browser.open(info_size)
+                # for f in browser.forms():  ##有的页面有很多表单，你可以通过来查看
+                #     print "#"*10
+                #     print str(f)
+                #     print "#"*10
+                browser.select_form(nr=1)
                 browser['zkzh'] = exam_num
                 browser['xm'] = student_name
                 browser.submit()
-                print browser.viewing_html
-
-
-
+                result=browser.response().read().find('error')
+                print 'ExamNo:%s result:%s' %(str(exam_num),result)
+                if result<0:
+                    print 'Finaly result is:'+exam_num
+                    break 
+                browser.back()
+                time.sleep(0.3)
+                #for f in browser.forms():  ##有的页面有很多表单，你可以通过来查看
+                #    print "#"*10
+                #    print str(f)
+                #    print "#"*10
 
 
 if __name__ == '__main__':
