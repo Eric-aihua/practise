d318eb5247ae3d3b41eb0e70fde4f13144a4245e
Eric-aihua <eric.sunah@gmail.com>
2017-08-25 15:02:15 +0800

    使用EWMA计算一段时间内告警日志的平均数量

--- java_cookbook/storm_demo/src/main/java/com/eric/storm/trident/windows/outbreakdetector/DispatchAlter.java
+++ java_cookbook/storm_demo/src/main/java/com/eric/storm/trident/windows/outbreakdetector/DispatchAlter.java
@@ -1,14 +1,10 @@
 package com.eric.storm.trident.windows.outbreakdetector;
 
 import org.apache.storm.trident.operation.BaseFunction;
-import org.apache.storm.trident.operation.Function;
 import org.apache.storm.trident.operation.TridentCollector;
-import org.apache.storm.trident.operation.TridentOperationContext;
 import org.apache.storm.trident.tuple.TridentTuple;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Map;
 
 public class DispatchAlter extends BaseFunction {
     private static final long serialVersionUID = -6619782418545145108L;
